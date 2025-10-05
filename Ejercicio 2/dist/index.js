ffa3c660e0d2574d82edd4ca5d39abc2c66d0316
MrPanda78 <santidj28@gmail.com>
2025-10-05 13:11:52 -0300

    Removed folder "dist"
    Unnecessary folder removed

--- Ejercicio 2/dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Menu_1 = require("./ui/Menu");
-(0, Menu_1.showMainMenu)(); // Llamar menú principal al iniciar código
